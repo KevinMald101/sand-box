bc7bd96992f594e93a324f98c1ba0c31997cd012
Kevin M <93846113+KevinMald101@users.noreply.github.com>
2023-05-09 19:58:17 -0400

    Toggling Website Themes
    Testing out themes using Javascript.

---
+++ dark_theme/change.js
@@ -0,0 +1,28 @@
+function dark() {
+  document.body.style.backgroundColor = "black";
+}
+
+function light() {
+  document.body.style.backgroundColor = "white";
+}
+
+function grey() {
+  document.body.style.backgroundColor = "grey";
+}
+
+/*
+function toggleDarkMode() {
+  const darkMode = document.getElementById("darkMode");
+  const lightMode = document.getElementById("lightMode");
+  const darkModeClass = darkMode.className;
+  const lightModeClass = lightMode.className;
+  
+  if (darkModeClass.indexOf("darkMode") > -1) {
+    darkMode.className = "lightMode";
+    lightMode.className = "darkMode";
+  } else {
+    darkMode.className = "darkMode";
+    lightMode.className = "lightMode";
+  }
+}
+*/
